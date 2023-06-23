3be529beba41f83778034108cfb56c62f06e1249
shenuka-jayasinghe <shenukacj@outlook.com>
2023-06-23 01:26:20 +0100

    added .env to hide API key

---
+++ environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiKey: 'Bearer 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'
+  };
+  
---
+++ src/app/shared/environmnt.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiKey: 'insert API key here'
+  };
